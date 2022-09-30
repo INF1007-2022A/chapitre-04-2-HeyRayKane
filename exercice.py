efbb118bdad7aad2d67ce7b6f7b9682e5de7f613
HeyRayKane <rayan.ajakane@polymtl.ca>
2022-09-29 23:41:34 -0400

    Complétion de l'exercice 4.2
    Par contre, le corrigé a une méthode 3 fois plus simple pour les fonctions encrypt et decrypt...

--- exercice.py
+++ exercice.py
@@ -4,16 +4,50 @@
 import random
 
 def get_first_part_of_name(name):
-	return ""
+	a= (name.capitalize()).split("-")
+	return a[0]
 
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+	random_animal = animals[random.randrange(len(animals))]
+	random_adjective = adjectives[random.randrange(len(adjectives))]
+	random_fruits = fruits[random.randrange(len(fruits))]
+	return f"Aujourd'hui, j'ai vu un {random_animal} s'emparer d'un panier {random_adjective} plein de {random_fruits}."
 
 def encrypt(text, shift):
-	return ""
+	encrypted_text = ""
+	shift %= 26
+	for i in text:
+		if i.isupper():
+			if (ord(i) + shift) > ord("Z"):
+				encrypted_text += chr(ord(i)-26+shift)
+			else:
+				encrypted_text += chr(ord(i)+shift)
+		elif i.islower():
+			if (ord(i) + shift) > ord("z"):
+				encrypted_text += chr(ord(i)-26+shift)
+			else:
+				encrypted_text += chr(ord(i)+shift)
+		else:
+			encrypted_text += i
+	return encrypted_text.upper()
 
 def decrypt(encrypted_text, shift):
-	return ""
+	decrypted_text = ""
+	shift %= 26
+	for i in encrypted_text:
+		if i.isupper():
+			if (ord(i) - shift) < ord("A"):
+				decrypted_text += chr(ord(i)+26-shift)
+			else:
+				decrypted_text += chr(ord(i)-shift)
+		elif i.islower():
+			if (ord(i) - shift) > ord("a"):
+				decrypted_text += chr(ord(i)+26-shift)
+			else:
+				decrypted_text += chr(ord(i)-shift)
+		else:
+			decrypted_text += i
+	return decrypted_text.lower()
 
 
 if __name__ == "__main__":
